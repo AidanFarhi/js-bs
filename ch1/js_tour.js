98ddbae7a72c168114a90815a932f92c05c6bfbf
Aidan Farhi <aidanmcfarhi@gmail.com>
2025-09-01 16:18:59 -0400

    finished up js_tour file

--- ch1/js_tour.js
+++ ch1/js_tour.js
@@ -63,4 +63,110 @@ count++                         // Increment by 1
 count--                         // Decrement by 1
 count += 1                      // Add 2
 count *= 3                      // Multipy by 3
-console.lot(count)              // => 6
+console.log(count)              // => 6
+
+// Equality and relational operators test whether two values are equal,
+// unequal, less than, greater than, and so on
+let v1 = 10
+let v2 = 20
+v1 === v2                       // => false
+v1 !== v2                       // => true
+v1 < v2                         // => true
+v1 <= v2                        // => true
+v1 > v2                         // => false
+v1 >= v2                        // => false
+"two" === "three"               // => false
+"two" > "three"                 // => true
+false === (v1 > v2);            // => true
+
+// Logical operators combine or invert boolean values
+(v1 === 2) && (v2 === 3)        // => true
+(v1 > 3) || (v2 < 3)            // => false
+!(v1 === v2)                    // => true
+
+// Functions are parameterized blocks of JavaScript code that we can invoke
+function plus1(x) {
+    return x + 1
+}
+plus1(10)                       // => 11
+
+let square = function(x) {
+    return x * x
+}
+square(plus1(2))                // => 9
+
+// Arrow functions
+const plus2 = x => x + 2
+const squareArrow = x => x * x
+plus2(10)                       // => 12
+squareArrow(plus2(1))           // => 9
+
+// When functions are assigned to the properties of an object,
+// they become 'methods'
+let a = []
+a.push(1, 2, 3)
+a.reverse()
+console.log(a)
+
+// We can define our own methods too. The 'this' keyword refers to
+// the object on which the method is defined
+points.dist = function() {
+    let p1 = this[0]
+    let p2 = this[1]
+    let a = p2.x - p1.x
+    let b = p2.y - p1.y
+    return Math.sqrt(a*a + b*b)
+}
+points.dist()                   // => 1.4142135623730951
+
+// Some functions illustrating common JavaScript control structure
+// statements
+function abs(x) {
+    if (x >= 0) {
+        return x
+    }
+    return -x
+}
+abs(-10) === abs(10)            // => true
+
+function sum(array) {
+    let sum = 0
+    for (let x of array) {
+        sum += x
+    }
+    return sum
+}
+sum(primes)                     // => 28
+
+function factorial(n) {
+    let product = 1
+    while (n > 1) {
+        product *= n
+        n--
+    }
+    return product
+}
+factorial(4)                    // => 24
+
+function factorial2(n) {
+    let i, product = 1
+    for (i = 2; i <= n; i++) {
+        product *= i
+    }
+    return product
+}
+factorial2(5)                   // => 120
+
+// Classes
+class Point {
+    constructor(x, y) {
+        this.x = x
+        this.y = y
+    }
+
+    distance() {
+        return Math.sqrt(this.x * this.x + this.y * this.y)
+    }
+}
+let p = new Point(1, 1)
+p.distance()                    // => 1.4142135623730951
