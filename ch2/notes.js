7f3e671126a4f9f1e12d895adc8989b3fab9ba80
Aidan Farhi <aidanmcfarhi@gmail.com>
2025-09-09 21:38:16 -0400

    added ch2

---
+++ ch2/notes.js
@@ -0,0 +1,60 @@
+// This is a single-line comment.
+
+/* This is also a single line comment */
+
+/**
+ * This is a multi-line comment.
+ * The extra stars are not required.
+ */
+
+/**
+ * Literals.
+ */
+
+12              // The number 12
+1.2             // The number one point 2
+"hello world"   // A string of text
+'Hi'            // Another string of text
+true            // A Boolean value
+false           // The other Boolean value
+null            // Absence of an object
+
+/**
+ * Identifiers.
+ * 
+ * Identifiers must meet the following criteria:
+ * - begin with a letter      'x'
+ * - begin with an underscore  _
+ * - begin with a dollar sign  $
+ * - subsequent characters can be letters, digits, underscores, or dollar signs.
+ */
+
+i
+my_variabl_name
+v13
+_dummy
+$str
+
+/**
+ * Reserved keywords.
+ * 
+ * as      const      export     get         null     target   void
+ * async   continue   extends    if          of       this     while
+ * await   debugger   false      import      return   throw    with
+ * break   default    finally    in          set      true     yield
+ * case    delete     for        instanceof  static   try
+ * catch   do         from       let         super    typeof
+ * class   else       function   new         switch   var
+ */
+
+/**
+ * Unicode.
+ * 
+ * JavaScript programs are written using the Unicode character set.
+ */
+
+const π = 3.14
+const sí = true
+let café = 1;
+caf\u00e9 = 1;
+caf\u{E9} = 100
